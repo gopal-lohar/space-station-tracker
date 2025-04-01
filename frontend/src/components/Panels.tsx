f900cc06bba03c772c84ea1a063885392d045fe3
Gopal <gopal.lohar.dev@gmail.com>
2025-04-01 15:48:03 +0530

    make location detection(probably working) work

--- frontend/src/components/Panels.tsx
+++ frontend/src/components/Panels.tsx
@@ -3,25 +3,142 @@ import { LeftPanel } from "./LeftPanel";
 import PassDetails from "./PassDetails";
 import PassesPanel from "./PassesPanel";
 import { Pass, queries } from "../queries";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const LOCATION_KEY = "ss_detector_location";
+
+type LocationState = {
+  latitude: number;
+  longitude: number;
+} | null;
 
 export default function Panels() {
-  const passesQuery = useQuery(queries.passesQuery);
+  const [location, setLocation] = useLocalStorage<LocationState>(
+    LOCATION_KEY,
+    null,
+  );
   return (
     <div className="grid gap-8 h-[calc(100%-5rem)] pb-4 grid-cols-3">
       <LeftPanel />
-      {passesQuery.error ? (
-        <div className="col-span-2">Error Loading Data</div>
-      ) : passesQuery.isLoading ? (
-        <div className="col-span-2">Loading...</div>
-      ) : !passesQuery.data ? (
-        <div className="col-span-2">Something Went Wrong</div>
-      ) : passesQuery.data.passes.length === 0 ? (
-        <div className="col-span-2">No Passes</div>
+      {location ? (
+        <Passes location={location} />
       ) : (
-        <TheTwoPanels passes={passesQuery.data.passes} />
+        <LocationPanel setLocation={setLocation} />
       )}
     </div>
+  );
+}
+
+function LocationPanel({
+  setLocation,
+}: {
+  setLocation: (value: LocationState) => void;
+}) {
+  const [localLocation, setLocalLocation] = useState({
+    latitude: 26.22983,
+    longitude: 78.17337,
+  });
+  const detectLocation = useCallback(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.error("User denied the request for geolocation");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.error("Location information is unavailable");
+              break;
+            case error.TIMEOUT:
+              console.error("The request to get user location timed out");
+              break;
+            default:
+              console.error("An unknown error occurred");
+              break;
+          }
+        },
+      );
+    } else {
+      console.log("nah, we got geolocation");
+    }
+  }, [setLocation]);
+  return (
+    <div>
+      <h2>Location</h2>
+      <div className="flex flex-col items-center gap-2 py-4">
+        <button
+          className="bg-cyan-700 py-2 px-4 rounded-md cursor-pointer"
+          onClick={detectLocation}
+        >
+          Detect Automatically
+        </button>
+        <div>OR</div>
+        <button
+          className="bg-cyan-700 py-2 px-4 rounded-md cursor-pointer"
+          onClick={() => {
+            setLocation({
+              latitude: localLocation.latitude,
+              longitude: localLocation.longitude,
+            });
+          }}
+        >
+          Enter Coordinates Manually
+        </button>
+        <div className="grid grid-cols-[10ch_1fr] items-center">
+          <label>Latitude</label>
+          <input
+            className="outline-none focus-visible:border-gray-700 border-2 border-gray-900 px-4 py-2"
+            value={localLocation.latitude}
+            onChange={(e) =>
+              setLocalLocation({
+                ...localLocation,
+                latitude: parseFloat(e.target.value),
+              })
+            }
+          />
+        </div>
+        <div className="grid grid-cols-[10ch_1fr] items-center">
+          <label>Longitude</label>
+          <input
+            className="outline-none focus-visible:border-gray-700 border-2 border-gray-900 px-4 py-2"
+            value={localLocation.longitude}
+            onChange={(e) =>
+              setLocalLocation({
+                ...localLocation,
+                longitude:
+                  parseFloat(e.target.value) || localLocation.longitude,
+              })
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Passes({
+  location,
+}: {
+  location: { latitude: number; longitude: number };
+}) {
+  const passesQuery = useQuery(queries.passesQuery(location));
+  return passesQuery.error ? (
+    <div className="col-span-2">Error Loading Data</div>
+  ) : passesQuery.isLoading ? (
+    <div className="col-span-2">Loading...</div>
+  ) : !passesQuery.data ? (
+    <div className="col-span-2">Something Went Wrong</div>
+  ) : passesQuery.data.passes.length === 0 ? (
+    <div className="col-span-2">No Passes</div>
+  ) : (
+    <TheTwoPanels passes={passesQuery.data.passes} />
   );
 }
 
