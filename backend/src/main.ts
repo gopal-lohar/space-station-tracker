9dfd19dcf1f27a06ae8206d88b3cebf152c4494f
Gopal <gopal.lohar.dev@gmail.com>
2025-04-13 00:34:38 +0530

    scrap previous routes and directly app.use new controllers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,110 +1,19 @@
 import { config } from "./config/env";
+import { tleRoutes } from "./routes/tle.routes";
 
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import {
-  calculateStateVector,
-  computePasses,
-  getSunTimes,
-} from "space-station-tracker-core";
 import { tleManager } from "./services/TleManager";
-
-// Define interfaces
-interface SSPosition {
-  time: number;
-  latitude: number;
-  longitude: number;
-}
-
-interface Pass {
-  startingTime: string;
-  endingTime: string;
-  startElevation: number;
-  maxElevation: number;
-  endElevation: number;
-  startDirection: number;
-  endDirection: number;
-  magnitude: number;
-}
-
-interface PassesResponse {
-  time: number;
-  passes: Pass[];
-}
+import { passesRoutes } from "./routes/passes.routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/tle", tleRoutes);
+app.use("/api/passes", passesRoutes);
+
 const PORT = config.server.port;
-
-app.get("/api/ss-position", async (req, res) => {
-  const { data: tle, error } = await tleManager.getTle(config.noradIds.iss);
-  if (!tle || error) {
-    res.status(500).json({ error: "Error fetching TLE data" });
-    return;
-  }
-
-  const stateVectors = calculateStateVector(new Date(), tle);
-
-  if (typeof stateVectors !== "object") {
-    res.status(500).json({ error: "Error calculating state vectors" });
-    return;
-  }
-
-  const ssPosition: SSPosition = {
-    time: Date.now(),
-    latitude: stateVectors.geodetic.position.latitude,
-    longitude: stateVectors.geodetic.position.longitude,
-  };
-
-  res.json(ssPosition);
-});
-
-app.get("/api/passes", async (req, res) => {
-  const latitude = Number(req.query.latitude);
-  const longitude = Number(req.query.longitude);
-
-  // Validate query parameters
-  if (isNaN(latitude) || isNaN(longitude)) {
-    res
-      .status(400)
-      .json({ error: "Valid latitude and longitude are required" });
-    return;
-  }
-
-  // by default iss
-  const { data: tle, error } = await tleManager.getTle(config.noradIds.iss);
-  if (!tle || error) {
-    res.status(500).json({ error: "Error fetching TLE data" });
-    return;
-  }
-
-  let passes = [] as Pass[];
-
-  if (tle) {
-    passes = computePasses({
-      delta: 1000 * 30,
-      observerLocation: { latitude, longitude, elevation: 0 },
-      startTime: new Date(new Date().toDateString()), // today at midnight
-      endTime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 30 days from now
-      tle,
-    });
-  }
-
-  const passesData: PassesResponse = {
-    time: Date.now(),
-    passes,
-  };
-
-  res.json(passesData);
-});
-
-app.get("/", function (req: Request, res: Response) {
-  res.send(
-    "Space Station API - Use /api/ss-position or /api/passes?latitude=X&longitude=Y",
-  );
-});
 
 async function main() {
   let error = await tleManager.updateTLEData();
@@ -112,7 +21,6 @@ async function main() {
     console.error("Error updating TLE data:", error);
     return;
   }
-
   // Start the server
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
